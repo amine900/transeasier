81aed61f7b3e21097ce8287f1fa18ee64aabe8ea
WassimSlim21 <WassimSlim18@gmail.com>
2021-09-13 01:09:18 +0100

    aaa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, HostListener, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from './services/auth.service';
 
 @Component({
@@ -10,8 +11,29 @@ export class AppComponent implements OnInit{
   screenWidth: number;
   title = 'transeasier';
   showdrop:boolean = false
-  constructor(public auth: AuthService) {
+  constructor(public auth: AuthService, private router : Router) {
     this.getScreenSize();
+    this.router.events.subscribe((val) =>
+    {
+      console.log(val);
+
+      let footer = document.getElementById('footer');
+      if(this.router.url == "/changalni" || this.router.url=="/publicTransit/bus" || this.router.url=="/publicTransit/metro" || this.router.url=="/publicTransit/TGM"){
+        footer.style.position = "fixed";
+        footer.style.width = "100%";
+      }
+      else {
+        footer.style.position = "static";
+        footer.style.width = "100%";
+        footer.style.bottom = "0";
+        console.log("hani fil else");
+
+      }
+
+    }
+    )
+
+
   }
 
   @HostListener('window:resize', ['$event'])
@@ -22,5 +44,6 @@ export class AppComponent implements OnInit{
     this.showdrop = !this.showdrop
   }
   ngOnInit(): void {
+
   }
 }
